6ec51e2e7ed6b8635845cad5af88006febe9058f
Berk <D00225745@student.dkit.ie>
2021-04-03 19:29:16 +0100

    new interfacec and files update v3 student

--- src/main/java/com/dkit/oopca5/BusinessObjects/App.java
+++ src/main/java/com/dkit/oopca5/BusinessObjects/App.java
@@ -2,22 +2,24 @@ package com.dkit.oopca5.BusinessObjects;
 
 import com.dkit.oopca5.DAO.MySqlStudentDao;
 import com.dkit.oopca5.DAO.StudentDaoInterface;
+import com.dkit.oopca5.DTO.Student;
 import com.dkit.oopca5.Exceptions.DaoException;
 
 public class App
 {
     public static void main(String[] args)
     {
-        StudentDaoInterface studentDao = new MySqlStudentDao();
+        // load students
+        StudentManager studentManager = new StudentManager();
 
-        try
-        {
-            List<Student> studentList = studentDao.findAllStudents();
-            System.out.println(studentList);
-        }
-        catch(DaoException e)
-        {
-            e.printStackTrace();
-        }
+        Student s = studentManager.getStudent(12345678);
+
+        int caoNum = 22223333;
+        String dob = "1999-11-26";
+        String pw = "w3e4r5";
+        studentManager.addStudent(new Student(caoNum,dob,pw));
+
+        System.out.println("Student: 12345678:" + s);
+
     }
 }
--- src/main/java/com/dkit/oopca5/BusinessObjects/StudentManager.java
+++ src/main/java/com/dkit/oopca5/BusinessObjects/StudentManager.java
@@ -23,29 +23,26 @@ public class StudentManager {
 
                 System.out.println("studentMap dump: " + studentsMap);
             }
+        } catch (DaoException e) {
+            e.printStackTrace();
         }
-        catch(DaoException e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-
-
-        // load from text file "students.dat" and populate studentsMap
-
-        // Hardcode some values to get started
-  //      studentsMap.put(22224444, new Student(22224444, "1999-03-15", "Annie$101", "annie@gmail.com"));
-  //      studentsMap.put(50501111, new Student(50501111, "2003-12-25", "banAnna$%", "pat.crean@yahoo.com"));
-  //      studentsMap.put(10103333, new Student(10103333, "2002-04-01", "cats&dogs:)", "cat.stephens@digiweb.ie"));
-  //      studentsMap.put(33330000, new Student(33330000, "2000-07-04", "guessMe1984", "malone_j@icloud.com"));
-
     }
+
+
+    // load from text file "students.dat" and populate studentsMap
+
+    // Hardcode some values to get started
+    //      studentsMap.put(22224444, new Student(22224444, "1999-03-15", "Annie$101", "annie@gmail.com"));
+    //      studentsMap.put(50501111, new Student(50501111, "2003-12-25", "banAnna$%", "pat.crean@yahoo.com"));
+    //      studentsMap.put(10103333, new Student(10103333, "2002-04-01", "cats&dogs:)", "cat.stephens@digiweb.ie"));
+    //      studentsMap.put(33330000, new Student(33330000, "2000-07-04", "guessMe1984", "malone_j@icloud.com"));
+
+}
 
     public Student getStudent(Integer caoNumber) {
         Student student = studentsMap.get(caoNumber); // get a reference to student object (could be null)
-        Student clone=null;
-        if(student!=null)
+        Student clone = null;
+        if (student != null)
             clone = new Student(student);
         return clone;  // return a clone of the student
     }
