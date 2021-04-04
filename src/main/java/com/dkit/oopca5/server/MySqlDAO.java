d37cce0ab3d9e2fc32aec9b17b2ca207b6366a71
Berk <D00225745@student.dkit.ie>
2021-04-04 12:51:47 +0100

    entire reset due to stupid decisions and mistakes

--- src/main/java/com/dkit/oopca5/server/MySqlDAO.java
+++ src/main/java/com/dkit/oopca5/server/MySqlDAO.java
@@ -4,6 +4,27 @@ package com.dkit.oopca5.server;
 All of the database functionality should be here. You will need a DAO for each table that you are interacting with in the database
  */
 
+import java.sql.DriverManager;
+
 public class MySqlDAO
 {
+    public Connection getConnection() throws DaoException
+    {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/oop_ca5_berk_tatar_emmanuel_francis";
+        String username = "root";
+        String password = "";
+        Connection con = null;
+
+        try
+        {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, username, password);
+        }
+
+        catch (ClassNotFoundException exl)
+        {
+
+        }
+    }
 }
