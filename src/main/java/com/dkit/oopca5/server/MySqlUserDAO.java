81eb70a19e065d9601a2e8f1ef7810a4c0629d8c
Berk <D00225745@student.dkit.ie>
2021-04-04 13:31:41 +0100

    entire reset due to stupid decisions and mistakes

---
+++ src/main/java/com/dkit/oopca5/server/MySqlUserDAO.java
@@ -0,0 +1,76 @@
+package com.dkit.oopca5.server;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MySqlStudentDao extends MySqlDao implements StudentDaoInterface
+{
+    @Override
+    public List<Student> findAllStudents() throws DaoException
+    {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<Student> students = new ArrayList<>();
+
+        try
+        {
+            con = this.getConnection();
+
+            String query = "SELECT * FROM STUDENT WHERE STUDENTNAME =? AND PASSWORD =?";
+            ps = con.prepareStatement(query);
+
+            rs = ps.executeQuery();
+            while(rs.next())
+            {
+                int caoNumber = rs.getInt("caoNumber");
+                String dob = rs.getString("dateOfBirth");
+                String password = rs.getString("password");
+
+
+                Student u = new Student(caoNumber,dob, password);
+                students.add(stu);
+            }
+
+        }
+
+        catch (SQLException e)
+        {
+            throw new DaoException("findAllUsers() " + e.getMessage());
+        }
+
+        finally
+        {
+            try
+            {
+                if(rs != null)
+                {
+                    rs.close();
+                }
+
+                if(ps != null)
+                {
+                    ps.close();
+                }
+
+                if(con != null)
+                {
+                    freeConnection(con);
+                }
+            }
+
+            catch (SQLException e)
+            {
+                throw new DaoException("findAllUsers() " + e.getMessage());
+            }
+        }
+
+        return users;
+    }
+
+
+
+
+
+}
