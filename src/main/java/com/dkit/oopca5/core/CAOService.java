f8ba7d8086834564afbf53a520be5d0b577049df
Berk <D00225745@student.dkit.ie>
2021-04-18 19:05:30 +0100

    Deliverable 2 Rush

--- src/main/java/com/dkit/oopca5/core/CAOService.java
+++ src/main/java/com/dkit/oopca5/core/CAOService.java
@@ -6,12 +6,17 @@ package com.dkit.oopca5.core;
 //Berk Tatar D00225745
 
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class CAOService
 {
-    public static final int PORT_NUM = 50025;
+    public static final String BREAKING_CHARACTER = "%%";
+    public static final int PORT_NUM = 50007;
     public static final String HOSTNAME = "localhost";
 
-    public static final String BREAKING_CHARACTER = "%%";
+    public static final String TASK_SEPARATOR = "##";
 
     public static final String REGISTER_COMMAND = "REGISTER";
     public static final String SUCCESSFUL_REGISTER = "REGISTERED";
@@ -21,7 +26,7 @@ public class CAOService
     public static final String LOGGED_IN = "LOGGED IN";
     public static final String LOGIN_FAILED = "LOGIN FAILED";
 
-    public static final String LOGOUT_COMMAND = "LOGIN";
+    public static final String LOGOUT_COMMAND = "LOGOUT";
     public static final String LOGGED_OUT = "LOGGED OUT";
 
     public static final String DISPLAY_COURSE_COMMAND = "DISPLAY COURSE";
@@ -39,4 +44,69 @@ public class CAOService
     public static final String FAILED_UPDATE_CURRENT = "FAILED UPDATE CURRENT";
     public static final String SUCCESSFUL_UPDATE_CURRENT = "SUCCESSFUL UPDATE CURRENT";
 
+//    public static final String ADD_COMMAND = "add";
+//    public static final String SUCCESSFUL_ADD = "ADDED";
+//    public static final String FAILED_ADD = "FAILED";
+//
+//    public static final String REMOVE_COMMAND = "remove";
+//    public static final String SUCCESSFUL_REMOVE = "DELETED";
+//    public static final String FAILED_REMOVE = "NOT_FOUND";
+//
+//    public static final String VIEW_COMMAND = "viewAll";
+//    public static final String TASK_SEPARATOR = "##";
+//
+//    public static final String EXIT_COMMAND = "exit";
+//    public static final String SIGN_OFF = "GOODBYE";
+//
+//    public static final String TRYING_TO_HACK = "STOP TRYING TO HACK US";
+
+    //Finish correcting ca5 del one, John, 88888888888
+    //Read masters thesis, John, 999999999999
+    //Finish correcting ca5 del one%%John%%8888888888##Read masters thesis%%John%%9999999999
+
+    public static String flattenTaskList(List<Task> taskList)
+    {
+        if(!taskList.isEmpty())
+        {
+            String tasks = taskList.get(0).format();
+            for(int i=1; i < taskList.size(); i++)
+            {
+                tasks = tasks + CAOService.TASK_SEPARATOR + taskList.get(i).format();
+            }
+            return tasks;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    //This is the opposite of the previous method
+    //takes input that looks like Finish correcting ca5 del one%%John%%8888888888##Read masters thesis%%John%%9999999999
+    public static List<Task> recreateTaskList(String tasks)
+    {
+        List<Task> taskList = new ArrayList<>();
+        //Now taskStrings will contain - Finish correcting ca5 del one%%John%%8888888888 and Read masters thesis%%John%%9999999999
+        String[] taskStrings = tasks.split(CAOService.TASK_SEPARATOR);
+        for (String task : taskStrings)
+        {
+            //Finish correcting ca5 del one%%John%%8888888888
+            //components - Finish correcting ca5 del one, John,8888888888
+            String[] components = task.split(CAOService.BREAKING_CHARACTER);
+            if (components.length == 3)
+            {
+                try
+                {
+                    long deadlineTime = Long.parseLong(components[2]);
+                    Date deadline = new Date(deadlineTime);
+                    Task t = new Task(components[0], components[1], deadline);
+                    taskList.add(t);
+                } catch (NumberFormatException e)
+                {
+                    System.out.println("Deadline information is not a long. Skipping entry");
+                }
+            }
+        }
+        return taskList;
+    }
 }
---
+++ src/main/java/com/dkit/oopca5/core/Course.java
@@ -0,0 +1,34 @@
+package com.dkit.oopca5.core;
+
+public class Course {
+    private String courseid;
+    private int level;
+    private String courseTitle;
+    private String institution;
+
+    public String getCourseid() {
+        return courseid;
+    }
+    public void setCourseid(String courseid) {
+        this.courseid = courseid;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+    public String getInstitution() {
+        return institution;
+    }
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+}
